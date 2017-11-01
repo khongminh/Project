27eddfe31c7a4e38f7bf936a518a67978d3c6161
mrechcheo149162536 <mrechcheo149162536@gmail.com>
2017-11-01 10:35:20 +0700

    create template

---
+++ LearnBootstrap/LearnBootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnBootstrap.Startup))]
+namespace LearnBootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
